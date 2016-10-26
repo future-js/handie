22bf53b0789755dab2dfca5aaaef7163e9e471c5
Ourai Lin <ourairyu@gmail.com>
2016-10-26 23:19:31 +0800

    Import Tangram

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 "use strict";
 
 const gulp = require("gulp");
-const compass = require("gulp-compass");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
 
-gulp.task("compile-sass", function() {
-  gulp.src("src/**/*.scss").pipe(compass({
-    project: __dirname,
-    css: "dist",
-    sass: "src"
-  }));
+gulp.task("compile", function() {
+  gulp
+    .src("./src/layouts/dashboard/index.scss")
+    .pipe(sass({outputStyle: "expanded", noLineComments: true}).on("error", sass.logError))
+    .pipe(cssmin({keepSpecialComments: 0}))
+    .pipe(rename("layouts/dashboard.min.css"))
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("default", ["compile-sass"]);
+gulp.task("default", ["compile"]);
