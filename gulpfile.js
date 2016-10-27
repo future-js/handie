75b5c4eb3e52e16e231867d615b3b98748a87ce5
Ourai Lin <ourairyu@gmail.com>
2016-10-27 12:10:37 +0800

    Import Bootstrap and FontAwesome

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,14 @@ const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
 
 gulp.task("compile", function() {
-  gulp
-    .src("./src/layouts/dashboard/index.scss")
-    .pipe(sass({outputStyle: "expanded", noLineComments: true}).on("error", sass.logError))
-    .pipe(cssmin({keepSpecialComments: 0}))
-    .pipe(rename("layouts/dashboard.min.css"))
-    .pipe(gulp.dest("./dist"));
+  ["reset", "layouts/dashboard"].forEach(function( f ) {
+    gulp
+      .src(`./src/${f}/index.scss`)
+      .pipe(sass({outputStyle: "expanded", noLineComments: true}).on("error", sass.logError))
+      .pipe(cssmin({keepSpecialComments: 0}))
+      .pipe(rename(`${f}.min.css`))
+      .pipe(gulp.dest("./dist"));
+  });
 });
 
 gulp.task("default", ["compile"]);
