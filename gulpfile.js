7b938885067d9ecc23bf6c8c4705774a574206ec
Ourai Lin <ourairyu@gmail.com>
2016-10-30 15:35:58 +0800

    Change the logic of building file

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,13 @@ const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
 
 gulp.task("compile", function() {
-  [{
-    src: "layouts/sidebar-outside",
-    dist: "themes/sidebar-outside-gray"
-  }, {
-    src: "layouts/sidebar-inside",
-    dist: "themes/sidebar-inside-yellow"
-  }].forEach(function( f ) {
+  ["sidebar-outside", "sidebar-inside"].forEach(function( f ) {
     gulp
-      .src(`./src/${f.src}/_exports.scss`)
+      .src(`./src/layouts/${f}/_exports.scss`)
       .pipe(concat("index.scss"))
       .pipe(sass({outputStyle: "expanded", noLineComments: true}).on("error", sass.logError))
       .pipe(cssmin({keepSpecialComments: 0}))
-      .pipe(rename(`${f.dist}.min.css`))
+      .pipe(rename(`themes/${f}-default.min.css`))
       .pipe(gulp.dest("./dist"));
   });
 });
