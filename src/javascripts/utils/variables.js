49602076ee6fb71f61539f85c8de19280a50131b
Ourai Lin <ourairyu@gmail.com>
2017-04-07 15:15:27 +0800

    Generate on 2017-04-07 15:15:27

--- src/javascripts/utils/variables.js
+++
@@ -1,9 +0,0 @@
-let defaults = {};
-const utils = {};
-const SUPPORTS = {
-  BS_MODAL: $.fn.hasOwnProperty("modal"),
-  BS_TABLE: $.fn.hasOwnProperty("bootstrapTable"),
-  SELECT2: $.fn.hasOwnProperty("select2"),
-  H5FX: window.hasOwnProperty("H5F"),
-  MOMENTJS: window.hasOwnProperty("moment")
-};
